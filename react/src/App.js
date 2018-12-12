1c0e1b3a98c837022c696655e39c169c89f191d3
vikash <vikashraj144@gmail.com>
2018-12-12 10:33:15 +0530

    app component added

---
+++ react/src/App.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+
+import logo from './logo.svg';
+import './App.css';
+import Header from "./components/layout/Header";
+import Sidebar from './components/layout/Sidebar';
+import AboutUs from './components/AboutUs';
+import Home from './components/Home';
+import PageNotFound from './components/PageNotFound';
+
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+          <div>
+              <Header/>
+              <Sidebar/>
+              <div className={'rightContentContainer'}>
+                    <div className="leftContentContainer">
+                    <Switch>
+                        <Route path='/' component={Home} exact/>
+
+                        <Route path='/about-us' component={AboutUs} />
+                        <Route path='/contact-us' component={AboutUs} />
+                        <Route component={PageNotFound} />
+
+                    </Switch>
+                    </div>
+                </div>
+              
+          </div>
+      </Router>
+  );
+  }
+}
+
+export default App;
