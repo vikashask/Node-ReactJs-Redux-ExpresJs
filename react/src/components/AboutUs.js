562afdef4b505ac4be6acd21612d63cdc4b38092
vikash <vikashraj144@gmail.com>
2018-12-12 11:42:46 +0530

    aboutus component

---
+++ react/src/components/AboutUs.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class AboutUs extends React.Component {
+    render = () => {
+        return(
+            <div>
+                AboutUs
+            </div>
+        );
+    }
+}
+
+export default AboutUs;
