bc357d995fe1f5b2a2bfc83a6f86f73829281e27
vikash <vikashraj144@gmail.com>
2018-12-12 11:43:15 +0530

    home ocmponent

---
+++ react/src/components/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Home = () => {
+
+    let imgStyle = {
+        marginTop: 50,
+        width: 500,
+        textAlign: 'center'
+    };
+
+    return(
+        <div>
+            Home
+        </div>
+    );
+};
+
+export default Home;
