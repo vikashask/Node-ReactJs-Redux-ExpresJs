44a20e1288b031df59ce9e50935025ccf4f693a8
vikash <vikashraj144@gmail.com>
2018-12-12 10:36:50 +0530

    sidebar added

---
+++ react/src/components/layout/Sidebar.js
@@ -0,0 +1,31 @@
+import React from "react";
+import {NavLink} from 'react-router-dom';
+
+const Sidebar = () => {
+
+    const links = [
+        {name: 'About Us', url: '/about-us'},
+        {name: '404 Error', url: '/404Error'},
+
+    ];
+
+    let linksComponents = links.map((link, index) => {
+        return (
+            <li key={index} className={'nav'}>
+                <NavLink className={'navLink'} activeClassName={'activeNavLink'}
+                         to={link.url} exact>{link.name}</NavLink>
+            </li>
+        );
+    });
+
+    return (
+        <div className={'leftNavContainer'}>
+            <ul>
+                <li className='nav'><NavLink className={'navLink'} activeClassName={'activeNavLink'} to='/' exact>Home</NavLink></li>
+                {linksComponents}
+            </ul>
+        </div>
+    );
+};
+
+export default Sidebar;
