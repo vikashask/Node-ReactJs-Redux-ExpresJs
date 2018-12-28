ebe11f2814897ab327083575dddde5d50d210fa1
vikash <vikashraj144@gmail.com>
2018-12-28 22:18:35 +0530

    provider added

--- react/src/index.js
+++ react/src/index.js
@@ -4,5 +4,12 @@ import './style/index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Provider } from "react-redux";
+import configureStore from "./store/configureStore";
+
+ReactDOM.render(
+<Provider>
+    <App />
+</Provider>
+, document.getElementById('root'));
 registerServiceWorker();
