f6d6972ab6e062818ee76c79322916ab1b071d89
vikash <vikashraj144@gmail.com>
2018-12-31 22:57:11 +0530

    boot starpa added

--- react/src/index.js
+++ react/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './style/index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 import { Provider } from "react-redux";
 import configureStore from "./store/configureStore";
