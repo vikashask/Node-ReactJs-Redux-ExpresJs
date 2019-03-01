2c3b22b50666c727277f7a4d94edd9cfc786a3d3
vikash <vikashraj144@gmail.com>
2019-03-01 11:50:30 +0530

    contact us com added

---
+++ react/src/components/ContactUs.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class ConactUs extends React.Component {
+
+    render = () =>{
+        return(
+            <div>
+                Contact Us
+            </div>
+        )
+    }
+}
+
+export default ConactUs;
