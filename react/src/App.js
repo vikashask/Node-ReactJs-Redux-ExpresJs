609a166002fa0c00acbb73b74d2c706624b35cc4
vikash <vikashraj144@gmail.com>
2019-03-06 18:32:29 +0530

    change in app

--- react/src/App.js
+++ react/src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
-import logo from './logo.svg';
 import './App.css';
 import Header from "./components/layout/Header";
 import Sidebar from './components/layout/Sidebar';
 import AboutUs from './components/AboutUs';
+import ContactUs from './components/ContactUs'
 import Home from './components/Home';
 import PageNotFound from './components/PageNotFound';
 
@@ -21,11 +21,10 @@ class App extends Component {
                     <div className="leftContentContainer">
                     <Switch>
                         <Route path='/' component={Home} exact/>
-
                         <Route path='/about-us' component={AboutUs} />
-                        <Route path='/contact-us' component={AboutUs} />
+                        <Route path='/about-us/profile' component={ContactUs} />
+                        <Route path='/contact-us' component={ContactUs} />
                         <Route component={PageNotFound} />
-
                     </Switch>
                     </div>
                 </div>
